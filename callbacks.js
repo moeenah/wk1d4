454dbc6dc5bed7017344d2a4f5d9c0fa142f8e93
moeenah <moeen.ahmad97@hotmail.com>
2018-05-10 20:01:16 +0000

    implemented foreach loop

--- callbacks.js
+++ callbacks.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  arr.forEach(function(name, i) {
+    if (name === "Waldo") {
       found(i);   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
