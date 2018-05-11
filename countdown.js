12deaaddbcdd3b944f6cd1f18e9c8dc5bb4e0378
moeenah <moeen.ahmad97@hotmail.com>
2018-05-11 02:02:14 +0000

    finished assignment

---
+++ countdown.js
@@ -0,0 +1,29 @@
+var countdownGenerator = function (x) {
+
+  return function() {
+    if (x > 0) {
+
+      console.log("T-minus " + (x));
+
+    }
+
+    else if (x === 0) {
+      console.log("Blast Off!");
+
+    }
+
+    else if ((x) < 0) {
+      console.log("Rockets already gone, bub!");
+
+    }
+    return x--;
+  }
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
